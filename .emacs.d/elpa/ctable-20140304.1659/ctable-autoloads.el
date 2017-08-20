eb30b8e67f937baa7756211c58de3a5c667d8af2
Raymond LeClair <raymondleclair@Revere.domain>
2017-08-20 11:18:23 -0500

    Removed unneeded directory, and its contents.

--- .emacs.d/elpa/ctable-20140304.1659/ctable-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; ctable-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/ctable-20140304.1659/ctable-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/ctable-20140304.1659/ctable.el")
-;;;;;;  (21873 64654 467729 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ctable-autoloads.el ends here
--- .emacs.d/elpa/epc-20140609.2234/epc-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; epc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/epc-20140609.2234/epc-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/epc-20140609.2234/epc-pkg.el"
-;;;;;;  "../../../../../.emacs.d/elpa/epc-20140609.2234/epc.el" "../../../../../.emacs.d/elpa/epc-20140609.2234/epcs.el")
-;;;;;;  (21873 64660 562666 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epc-autoloads.el ends here
