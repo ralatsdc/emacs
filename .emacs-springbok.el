302297ec0fda474609d026caeff659e310e986cf
Raymond LeClair <raymondleclair@Revere.domain>
2017-08-20 11:23:27 -0500

    Added final emacs configuration.

--- .emacs-springbok.el
+++ .emacs-springbok.el
@@ -13,4 +13,29 @@
 
 (load (concat emacs-home ".emacs-fini.el"))
 
-(load "/Users/raymondleclair/Projects/Development/emacs/.emacs-fini.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dired-use-ls-dired nil)
+ '(electric-pair-mode 1)
+ '(ess-default-style (quote DEFAULT))
+ '(ess-keep-dump-files "always")
+ '(ess-use-auto-complete t)
+ '(indent-tabs-mode nil)
+ '(inferior-ess-client-command "Initial")
+ '(inferior-ess-client-name "Initial")
+ '(inferior-ess-ddeclient "Initial")
+ '(inhibit-startup-screen t)
+ '(jdee-server-dir "~/.emacs.d/jdee-server/jars")
+ '(matlab-indent-level 2)
+ '(package-selected-packages
+   (quote
+    (helm-ag helm yasnippet web-beautify tern python-mode magit js2-mode jedi jdee))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
