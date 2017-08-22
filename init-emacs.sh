8ccf6f32fac5b60f5cfe5599483e02ed617a1dda
Raymond LeClair <raymondleclair@Revere.domain>
2017-08-21 21:30:55 -0500

    Set environment for applications launched from the Finder using launchd in order to survive a restart.

--- init-emacs.sh
+++ init-emacs.sh
@@ -5,9 +5,13 @@ sudo port install emacs-app
 sudo port install emacs-devel
 
 # Abbrev-mode prerequisite
-mkdir -p ~/.emacs.d/abbrev_defs
+touch ~/.emacs.d/abbrev_defs
 
 # Set environment for applications launched from the Finder (Mac OS X)
+# See: http://www.dowdandassociates.com/blog/content/howto-set-an-environment-variable-in-mac-os-x-launchd-plist/
 set -e
-launchctl setenv EMACS_HOME $EMACS_HOME
-killall Dock
+pushd $SCRIPT_HOME
+sed s:{{EMACS_HOME}}:$EMACS_HOME: org.gnu.emacs.plist > org.gnu.emacs.plist.1
+sudo mv org.gnu.emacs.plist.1 /Library/LaunchDaemons/org.gnu.emacs.plist
+launchctl load org.gnu.emacs.plist
+popd
