dc174fb3b26560a3312e521b5f6ecba7c745905b
Raymond LeClair <raymond.leclair@addgene.org>
2021-03-28 21:28:17 -0400

    Delete Addgene related files

--- init-addgene.el
+++
@@ -1,7 +0,0 @@
-(setq emacs-home (file-name-as-directory "{{EMACS_HOME}}"))
-
-;; Initialize packages required for Python and JavaScript development
-(load (concat emacs-home "init-emacs.el"))
-(load (concat emacs-home "init-development.el"))
-(load (concat emacs-home "init-python.el"))
-(load (concat emacs-home "init-javascript.el"))
