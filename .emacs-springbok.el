864e38d7e8b7c2e8aa506b87a6eada6b6e3deb14
Raymond LeClair <raymond.leclair@addgene.org>
2021-03-28 21:35:51 -0400

    Clean up organization init file

--- .emacs-springbok.el
+++ .emacs-springbok.el
@@ -2,42 +2,36 @@
 
 ;; Load packages for Python, JavaScript, MATLAB, Java, and R
 ;; development
-(package-initialize)
-
 (load (concat emacs-home ".emacs-init.el"))
-
 (load (concat emacs-home ".emacs-development.el"))
 (load (concat emacs-home ".emacs-python.el"))
 (load (concat emacs-home ".emacs-javascript.el"))
 (load (concat emacs-home ".emacs-matlab.el"))
-;; (load (concat emacs-home ".emacs-java.el"))
+(load (concat emacs-home ".emacs-java.el"))
 (load (concat emacs-home ".emacs-r.el"))
+(load (concat emacs-home ".emacs-scala.el"))
 (load (concat emacs-home ".emacs-yaml.el"))
-
 (load (concat emacs-home ".emacs-fini.el"))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
+ ;; Your init file should contain only one custom-set-variables instance.
  ;; If there is more than one, they won't work right.
  '(dired-use-ls-dired nil)
+ '(default-fill-column 120)
  '(electric-pair-mode 1)
  '(ess-default-style (quote DEFAULT))
  '(ess-keep-dump-files "always")
  '(ess-use-auto-complete t)
+ '(js-indent-level 4)
  '(indent-tabs-mode nil)
  '(inferior-ess-client-command "Initial")
  '(inferior-ess-client-name "Initial")
  '(inferior-ess-ddeclient "Initial")
  '(inhibit-startup-screen t)
- ;; '(jdee-server-dir "~/.emacs.d/jdee-server/jars")
- '(matlab-indent-level 2)
+ '(matlab-indent-level 4)
  '(package-selected-packages
    (quote
-    ;; (helm-ag helm yasnippet web-beautify tern python-mode magit js2-mode jedi jdee))))
-    ;; (helm-ag helm yasnippet web-beautify tern python-mode magit js2-mode jedi eclim))))
-    (helm-ag helm yasnippet web-beautify tern python-mode magit js2-mode jedi))))
+    (flycheck auto-complete async exec-path-from-shell helm-ag helm yasnippet web-beautify tern python-mode magit js2-mode jedi))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
