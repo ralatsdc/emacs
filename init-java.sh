df1ddb71ef3d152228307162ad1f3bb1a5cd3941
Raymond LeClair <raymond.leclair@springbok.io>
2024-03-05 17:59:16 -0500

    Add openjdk

--- init-java.sh
+++ init-java.sh
@@ -2,12 +2,14 @@
 
 # System packages
 if [[ "$OSTYPE" == 'darwin21' ]]; then
+    sudo port install openjdk8
     sudo port install maven3
     sudo port select --set maven maven3
 
 elif [[ "$OSTYPE" == 'linux-gnu' ]]; then
+    sudo apt-get install openjdk8
     sudo apt-get install maven3
-    
+
 else
     echo "Unexpected OS"
     exit 1
